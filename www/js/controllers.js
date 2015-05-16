ee3ebca422c5296aecd2a5cd38685e104e202fc4
Seaborn Lee <seabornlee.cn@gmail.com>
2015-05-16 15:38:41 +0800

    Send and receive messagex

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,7 +2,10 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats, $location) {
+.controller('ChatsCtrl', function($scope) {
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, $location) {
   var user = $location.search().user;
   var appkey = "5556d21627302bb31589348e";
   var yunba = new Yunba({server: 'sock.yunba.io', port: 3000, appkey: appkey});
@@ -14,8 +17,6 @@ angular.module('starter.controllers', [])
           if (user == 1) {
             console.log('user = 1')
             subscribeTopic();
-          } else {
-            sendMessage();
           }
         } else {
           console.log(msg);
@@ -34,15 +35,15 @@ angular.module('starter.controllers', [])
         console.log(msg);
       }
     });
-
   };
 
   yunba.set_message_cb(function (data) {
     console.log('Topic:' + data.topic + ',Msg:' + data.msg);
   });
 
-  var sendMessage = function() {
-    yunba.publish({'topic': 'my_topic', 'msg': '你好！Yunba。'},
+  $scope.msg = {};
+  $scope.sendMessage = function() {
+    yunba.publish({'topic': 'my_topic', 'msg': $scope.msg.text},
       function (success, msg) {
         if (success) {
           console.log('消息发布成功');
@@ -51,16 +52,7 @@ angular.module('starter.controllers', [])
         }
       }
     );
-  };
-
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
   }
-})
-
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
 })
 
 .controller('AccountCtrl', function($scope) {
